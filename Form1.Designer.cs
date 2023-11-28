8468792a27f6bd997237ec3b719466560e064d07
wesleyepeneto <wesleyepeneto@hotmail.com>
2023-11-28 09:56:09 -0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace Ficha_FII_Fiagro
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvResultados = new System.Windows.Forms.DataGridView();
+            this.Mes = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvResultados
+            // 
+            this.dgvResultados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResultados.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Mes,
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10,
+            this.Column11});
+            this.dgvResultados.Location = new System.Drawing.Point(118, 103);
+            this.dgvResultados.Name = "dgvResultados";
+            this.dgvResultados.RowTemplate.Height = 25;
+            this.dgvResultados.Size = new System.Drawing.Size(1277, 622);
+            this.dgvResultados.TabIndex = 0;
+            this.dgvResultados.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Mes
+            // 
+            this.Mes.HeaderText = "Mês";
+            this.Mes.Name = "Mes";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Resultado líquido do mês";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Resultado negativo até o mês anterior";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Base de cálculo do imposto";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Prejuizo a compensar";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Alíquota do imposto";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Imposto devido";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Saldo do imposto retido nos meses anteriores (Lei 11.033/2004)";
+            this.Column7.Name = "Column7";
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Imposto retido no mes (Lei 11.033/2004)";
+            this.Column8.Name = "Column8";
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Imposto a compensar (Lei 11.033/2004)";
+            this.Column9.Name = "Column9";
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "Imposto a pagar";
+            this.Column10.Name = "Column10";
+            // 
+            // Column11
+            // 
+            this.Column11.HeaderText = "Imposto pago";
+            this.Column11.Name = "Column11";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1573, 752);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvResultados);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvResultados;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Mes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ficha_FII_Fiagro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            dgvResultados.Rows.Add(12);
+            preencherGridMes();
+            preencherResultadoLiquidoMes();
+            preencherResultadoNegativoAteoMesAnterior();
+            preencherBaseDeCalculoDoImposto();
+            preencherPrejuizoaCompensar();
+            preencherAliquotaDoImposto();
+            preencherImpostoDevido();
+            preencherSaldoImpostoRetidoMesesAnteriores();
+            preencherImpostoRetidoNoMes();
+            preencherImpostoaCompensar();
+            preencherImpostoaPagar();
+            preencherImpostoPago();
+
+
+            //bloqueios 
+
+            bloquearResultadoNegativoMesAnterior();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void preencherGridMes()
+        {
+            DateTime data = DateTime.Parse("01/01/" + DateTime.Now.ToShortDateString().Substring(6, 4));
+
+            CultureInfo cultura = new CultureInfo("pt-BR");
+
+            int numeroDeLinhas = dgvResultados.Rows.Count;
+
+            for (int i = 0; i < numeroDeLinhas && i < 12; i++)
+            {
+                // Obter o nome do mês em português
+                string nomeDoMes = data.ToString("MMMM", cultura).Substring(0,3).ToUpper();
+                dgvResultados.Rows[i].Cells[0].Value = nomeDoMes;
+
+                data = data.AddMonths(1);
+            }
+        }
+
+        public void preencherResultadoLiquidoMes()
+        {
+            int numeroDeLinhas = dgvResultados.Rows.Count;
+
+            for (int i = 0; i < numeroDeLinhas && i < 12; i++)
+            {
+                // Obter o nome do mês em português
+               
+                dgvResultados.Rows[i].Cells[1].Value = "0,00";
+
+            }
+        }
+
+        public void preencherResultadoNegativoAteoMesAnterior()
+        {
+            int numeroDeLinhas = dgvResultados.Rows.Count;
+
+            for (int i = 0; i < numeroDeLinhas && i < 12; i++)
+            {
+                // Obter o nome do mês em português
+
+                dgvResultados.Rows[i].Cells[2].Value = "0,00";
+
+            }
+        }
+
+        public void preencherBaseDeCalculoDoImposto()
+        {
+            int numeroDeLinhas = dgvResultados.Rows.Count;
+
+            for (int i = 0; i < numeroDeLinhas && i < 12; i++)
+            {
+                // Obter o nome do mês em português
+
+                dgvResultados.Rows[i].Cells[3].Value = "0,00";
+
+            }
+        }
+
+        public void preencherPrejuizoaCompensar()
+        {
+            int numeroDeLinhas = dgvResultados.Rows.Count;
+
+            for (int i = 0; i < numeroDeLinhas && i < 12; i++)
+            {
+                // Obter o nome do mês em português
+
+                dgvResultados.Rows[i].Cells[4].Value = "0,00";
+
+            }
+        }
+
+        public void preencherAliquotaDoImposto()
+        {
+            int numeroDeLinhas = dgvResultados.Rows.Count;
+
+            for (int i = 0; i < numeroDeLinhas && i < 12; i++)
+            {
+                // Obter o nome do mês em português
+
+                dgvResultados.Rows[i].Cells[5].Value = "20,00";
+
+            }
+        }
+
+        public void preencherImpostoDevido()
+        {
+            int numeroDeLinhas = dgvResultados.Rows.Count;
+
+            for (int i = 0; i < numeroDeLinhas && i < 12; i++)
+            {
+                // Obter o nome do mês em português
+
+                dgvResultados.Rows[i].Cells[6].Value = "0,00";
+
+            }
+        }
+
+        public void preencherSaldoImpostoRetidoMesesAnteriores()
+        {
+            int numeroDeLinhas = dgvResultados.Rows.Count;
+
+            for (int i = 0; i < numeroDeLinhas && i < 12; i++)
+            {
+                // Obter o nome do mês em português
+
+                dgvResultados.Rows[i].Cells[7].Value = "0,00";
+
+            }
+        }
+        public void preencherImpostoRetidoNoMes()
+        {
+            int numeroDeLinhas = dgvResultados.Rows.Count;
+
+            for (int i = 0; i < numeroDeLinhas && i < 12; i++)
+            {
+                // Obter o nome do mês em português
+
+                dgvResultados.Rows[i].Cells[8].Value = "0,00";
+
+            }
+        }
+
+        public void preencherImpostoaCompensar()
+        {
+            int numeroDeLinhas = dgvResultados.Rows.Count;
+
+            for (int i = 0; i < numeroDeLinhas && i < 12; i++)
+            {
+                // Obter o nome do mês em português
+
+                dgvResultados.Rows[i].Cells[9].Value = "0,00";
+
+            }
+        }
+
+        public void preencherImpostoaPagar()
+        {
+            int numeroDeLinhas = dgvResultados.Rows.Count;
+
+            for (int i = 0; i < numeroDeLinhas && i < 12; i++)
+            {
+                // Obter o nome do mês em português
+
+                dgvResultados.Rows[i].Cells[10].Value = "0,00";
+
+            }
+        }
+
+        public void preencherImpostoPago()
+        {
+            int numeroDeLinhas = dgvResultados.Rows.Count;
+
+            for (int i = 0; i < numeroDeLinhas && i < 12; i++)
+            {
+                // Obter o nome do mês em português
+
+                dgvResultados.Rows[i].Cells[11].Value = "0,00";
+
+            }
+        }
+
+        public void calcularResultadoNegativoAcumulado()
+        {
+            // Supondo que o DataGridView seja chamado dgvResultados e as colunas 2 e 3 sejam a 1ª e 2ª colunas, respectivamente
+
+            // Inicialize o resultado negativo acumulado com o valor da primeira linha da coluna 3
+            decimal resultadoNegativoAcumulado = Convert.ToDecimal(dgvResultados.Rows[0].Cells[2].Value);
+
+            // Loop através das linhas do DataGridView a partir da segunda linha
+            for (int i = 0; i < dgvResultados.Rows.Count-1; i++)
+            {
+                dgvResultados.Rows[i].Cells[3].Value = "0";
+
+                // Obtenha o valor da coluna 1 (resultado líquido do mês anterior)
+                if(i == 0)
+                {
+                    decimal resultadoNegativoMesAnterior = Convert.ToDecimal(dgvResultados.Rows[i].Cells[2].Value);
+                }
+                else
+                {
+                    decimal resultadoNegativoMesAnterior = Convert.ToDecimal(dgvResultados.Rows[i - 1].Cells[2].Value);
+                }
+                
+
+                // Obtenha o valor da coluna 2 (resultado líquido do mês)
+                decimal resultadoLiquidoMes = Convert.ToDecimal(dgvResultados.Rows[i].Cells[1].Value);
+
+                if(resultadoLiquidoMes < 0)
+                {
+                    resultadoLiquidoMes = Math.Abs(resultadoLiquidoMes);
+
+                    // Calcule o resultado negativo acumulado até o mês anterior (coluna 3)
+                    dgvResultados.Rows[i].Cells[2].Value = resultadoNegativoAcumulado;
+
+                    // Atualize o resultado negativo acumulado para incluir o resultado líquido do mês atual
+                    resultadoNegativoAcumulado = resultadoNegativoAcumulado + resultadoLiquidoMes;
+                }
+                else
+                {
+                    resultadoLiquidoMes = Math.Abs(resultadoLiquidoMes);
+
+                    // Calcule o resultado negativo acumulado até o mês anterior (coluna 3)
+                    dgvResultados.Rows[i].Cells[2].Value = resultadoNegativoAcumulado;
+
+                    // Atualize o resultado negativo acumulado para incluir o resultado líquido do mês atual
+                    resultadoNegativoAcumulado = Math.Abs(resultadoNegativoAcumulado) - resultadoLiquidoMes;
+
+
+                    if(resultadoNegativoAcumulado < 0 )
+                    {
+                        dgvResultados.Rows[i].Cells[3].Value = Math.Abs(resultadoNegativoAcumulado);
+                        resultadoNegativoAcumulado = 0;
+                    }
+                   
+                }
+                // Use Math.Abs para obter o valor absoluto do resultado líquido do mês
+                
+            }
+
+        }
+
+        public void bloquearResultadoNegativoMesAnterior()
+        {
+            for (int i = 1; i < dgvResultados.Rows.Count; i++)
+            {
+                dgvResultados.Rows[i].Cells[2].ReadOnly = true;
+            }
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            calcularResultadoNegativoAcumulado();
+        }
+    }
+}
